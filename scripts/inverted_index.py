80f3355dc393de487ead35ac4ca004faaf348096
Leonardo Jose  Schettini de Arruda <a1601323@unet.univie.ac.at>
2017-09-04 19:05:50 -0300

    Refactor InvertedIndex and implement search in Ranker
    The term_indexer attributes was moved to Ranker class and the __init__ method is now a high order function that receives a function to process the corpus.

--- scripts/inverted_index.py
+++ scripts/inverted_index.py
@@ -1,17 +1,12 @@
 """
 Inverted index of the received documents.
-
-Contains 2 attributes to be used externally:
-    term_documents: {str: (int, {str:int})}
-    term_documents has as key a term (word) in the 
-
-    term_indexer: {str: int}
 """
 
 
 class InvertedIndex:
+    clean_function = None
+    processed_corpus = {}
     term_documents = {}
-    term_indexer = {}
 
     """
     Initializes the inverted index with the received corpus.
@@ -19,20 +14,22 @@ class InvertedIndex:
     :param corpus: [(str, [str])] list of document names and words
     """
 
-    def __init__(self, corpus):
-        self.__build_term_documents(corpus)
-        self.__build_term_indexer()
+    def __init__(self, corpus, clean_function = None):
+        self.clean_function = clean_function
+        self.__build_inverted_index(corpus)
 
     """
-    Creates the term_document dict based on the document_list.
+    Creates the term_document dict based on the document_list and process the corpus with the received clean function.
     
     :param corpus: [(str, [str])] list of document names and words
     """
 
-    def __build_term_documents(self, corpus):
+    def __build_inverted_index(self, corpus):
         for document in corpus:
             document_name = document[0]
-            document_words = document[1]
+            document_words = self.clean_function(document[1]) if self.clean_function is not None else document[1] 
+            self.processed_corpus[document_name] = document_words
+            
             for word in document_words:
                 if word not in self.term_documents:
                     self.term_documents[word] = (0, {})
@@ -43,13 +40,3 @@ class InvertedIndex:
                     self.term_documents[word][1]
                 )
                 self.term_documents[word][1][document_name] += 1
-
-    """
-    Creates the term_indexer, that is a map of each term to its respective index
-    """
-
-    def __build_term_indexer(self):
-        index = 0
-        for term in self.term_documents:
-            self.term_indexer[term] = index
-            index += 1
--- scripts/ranker.py
+++ scripts/ranker.py
@@ -1,34 +1,43 @@
 from inverted_index import InvertedIndex
-from math import log
+from math import log, sqrt
 
 class Ranker:
-    corpus = None
     inverted_index = None
+    term_indexer = {}
     document_vectors = {}
 
-    def __init__(self, inverted_index, corpus):
-        self.corpus = corpus
+    _whitespace_regex = re.compile(r'( |\n|[^A-Za-z0-9])+')
+
+    def __init__(self, inverted_index):
         self.inverted_index = inverted_index
+        self.__build_term_indexer()
         self.__calculate_document_vectors()
-
+    
+    def __build_term_indexer(self):
+        index = 0
+        for term in self.inverted_index.term_documents:
+            self.term_indexer[term] = index
+            index += 1
+            
     def __calculate_document_vectors(self):
-        for document in self.corpus:
-            document_name = document[0]
-            document_words = document[1]
-            self.document_vectors[document_name] = __calculate_document_tfidf(document_name)
+        for document_name in self.inverted_index.processed_corpus:
+            self.document_vectors[document_name] = __calculate_document_vector(document_name)
 
-    def __calculate_document_tfidf(self, document_name):
+    def __calculate_document_vector(self, document_name):
+        document_vector_mag2 = 0
         document_vector = len(self.inverted_index.term_documents.keys) * [0]
 
         for term in self.inverted_index.term_documents:
             (term_frequency, documents) = self.inverted_index.term_documents[term]
             tf = documents[document_name] / term_frequency # term frequency on document / total term frequency
-            idf = log(len(self.corpus) / len(documents))
-            document_vector[self.inverted_index.term_indexer[term]] = tf * idf
+            idf = log(len(self.inverted_index.processed_corpus) / len(documents))
+            tfidf = tf * idf
+            document_vector[self.term_indexer[term]] = tfidf
+            document_vector_mag2 += tfidf * tfidf
 
-        return document_vector
+        return (sqrt(document_vector_mag2), document_vector)
 
-    def __calculate_query_tfidf(self, query_terms):
+    def __calculate_query_vector(self, query_terms):
         query_vector = len(self.inverted_index.term_documents.keys) * [0]
 
         query_inverted_index = InvertedIndex([('query', query_terms)])
@@ -41,15 +50,30 @@ class Ranker:
             (term_frequency, documents) = self.inverted_index.term_documents[term]
 
             tf = query_term_frequency / term_frequency
-            idf = log(len(self.corpus) / len(documents))
+            idf = log(len(self.inverted_index.processed_corpus) / len(documents))
 
-            query_vector[self.inverted_index.term_indexer[term]] = tf * idf
+            query_vector[self.term_indexer[term]] = tf * idf
 
         return query_vector
 
     def search(self, query):
-        #aplica transf
-        #calcula tfidf
-        pass
-        
-        
+        query terms = self.inverted_index.clean_function(whitespace_regex.sub(' ', query).split(' '))
+        query_vector = __calculate_query_vector(query terms)
+
+        # compute similarity
+        similarities = []
+        for document_name in self.document_vectors:
+            (document_vector_mag, document_vector) = document_vectors[document_name]
+            similarity = self.list_similarity(query_vector, document_vector, document_vector_mag)
+            similarities.append((document_name, similarity))
+
+        similarities.sort(key = lambda tuple: tuple[1])
+        return similarities
+
+    def list_similarity(self, query_vector, document_vector, document_vector_mag):
+        dot_product = 0
+
+        for i in range(0, len(query_vector)):
+            dot_product += query_vector[i] * document_vector[i]
+
+        return dot_product / document_vector_mag
